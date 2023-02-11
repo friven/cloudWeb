5a7a4f088665c75f593b5ec54b8da94d255080eb
caofan <caofan@example.com>
2023-02-11 13:17:58 +0800

    补录

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <header class="titleBar">
+    <p class="title">生活倒影</p>
+    <div class="sideBar">
+      <div class="sideItem" v-for="item in NavigationBar" :key="item">
+        {{ item }}
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+const NavigationBar: string[] = reactive(['首页', '生活倒影', '聊天室', '留言板', '关于'])
+console.log(NavigationBar.length)
+console.log(NavigationBar)
+
+</script>
+
+<style lang="scss" scoped>
+.titleBar {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 60px;
+  font-size: 30px;
+  background-color: transparent;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+
+.titleBar:hover {
+  background-color: rgba(0, 0, 0, 0.5);
+  box-shadow: 0px 1px 3px 0px rgb(0, 34, 77);
+}
+
+.sideBar {
+  display: flex;
+  justify-content: space-around;
+  line-height: 60px;
+}
+
+.sideItem {
+  width: 150px;
+  // background-color: skyblue;
+  text-align: center;
+  color: #fff;
+}
+
+.title {
+  height: 60px;
+  line-height: 60px;
+  color: #fff;
+  margin-left: 20px;
+}
+
+.title:hover {
+  color: red;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="home">
+    <NavigationBar></NavigationBar>
+    <div class="backGroundImage">
+
+    </div>
+    <div class="message">
+      <p>料青山见我应如是</p>
+      <div class="yiyan">{{ paragraphTmp }}<span class="cursor">|</span></div>
+    </div>
+    <div>
+      <div class="wave1"></div>
+      <div class="wave2"></div>
+    </div>
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent, onBeforeUnmount, reactive, ref } from 'vue'
+import { Vue } from 'vue-class-component'
+import { testGetApi, yiyanAPI } from '../request/api'
+import NavigationBar from '@/components/NavigationBar.vue' // @ is an alias to /src
+
+// components: {
+//   NavigationBar
+// },
+const arr: string[] = ['首页', 'Tab1']
+const paragraph = ref<string>('') // 存储一言全部数据
+const paragraphTmp = ref<string>('')// 存储定时器定时增加数据
+let timer = 0
+let timerIndex = 0
+// const paraWidth = ref<string>('50px')
+const backGroundImageHome = ref<string>('../assets/image/bc1.jpg')
+const number = 0
+// const num1: number = number || 111 // 0默认也是undefined
+// const num2: number = number ?? 111
+// console.log('print num', num1, num2)
+// setup虽然自带async，但单独用await只能在顶层使用，如果在函数下还是要async / await一起写
+// let res = await indexAPI() //顶层直接使用
+// const search = async(val: IUseTableParam) => { //函数中用
+//     let res = await returnApplyListAPi({
+//         ...val,
+//     })
+//     let { list, pageNum, pageSize, total } = res.data
+// }
+// 每日一言接口及调用
+const yiyan = () => { // .then 使用回调函数
+  yiyanAPI().then((res) => {
+    console.log('***', res)
+    paragraph.value = res.toString()
+    startTimer()
+  })
+}
+yiyan()
+
+const startTimer = () => {
+  timer = setInterval(() => {
+    // 设置定时器
+    paragraphTmp.value += paragraph.value[timerIndex]
+    timerIndex++
+    if (timerIndex === paragraph.value.length) {
+      clearInterval(timer)
+      setTimeout(() => {
+        timerIndex = 0
+        paragraphTmp.value = ''
+        startTimer()
+      }, 3000)
+    }
+  }, 300)
+}
+
+onBeforeUnmount(() => {
+  clearInterval(timer) // 清除定时器
+  timer = 0
+})
+// export default class HomeView extends Vue {
+//   const arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// }
+</script>
+
+<style lang="scss" scoped>
+$imageBG: v-bind('backGroundImageHome');
+
+.backGroundImage {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background: center / cover no-repeat;
+  background-image: url($imageBG);
+}
+
+.message {
+  width: 100%;
+  height: 120px;
+  text-align: center;
+  padding: 400px 0 400px 0;
+  color: #fff;
+  font-size: 50px;
+  display: flex;
+  flex-direction: column;
+}
+
+.content {
+  width: 100%;
+  height: 1000px;
+  margin-top: 920px;
+  background-color: #fff;
+}
+
+.yiyan {
+  // width: v-bind('paraWidth');
+  height: 50px;
+  background-color: rgba($color: #2c2929, $alpha: 0.6);
+  border-radius: 10px;
+  align-self: center;
+  margin-top: 10px;
+  cursor: pointer;
+  color: #fff;
+  border-radius: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  line-height: 50px;
+  text-align: center;
+  font-size: 25px;
+}
+
+@keyframes hide {
+  to {
+    visibility: hidden;
+    opacity: 0;
+    transform: translateY(100px)
+  }
+}
+
+.cursor {
+  margin-left: 1px;
+  animation: hide .7s infinite;
+  font-weight: 400;
+  font-family: 'Courier New', Courier, monospace;
+}
+</style>
